1c6439ddfda824a1da0211d0d65a9c56d716513b
Seraiah Kutai <SeKutai@Ursinus.edu>
2019-05-09 15:46:03 -0400

    new node class

---
+++ Node.cpp
@@ -0,0 +1,73 @@
+#include <map>
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
+class Node{
+    private:
+        bool isTerminal;
+        string value;
+        map <string, Node*> hashMap;
+
+    public:
+        Node(string val) 
+        {
+            value = val;
+        }
+
+        string getValue()
+        {
+            return value;
+        }
+
+        void setValue(string val)
+        {
+            value = val;
+        }
+
+        void addNode(string val)
+        {
+            Node* node = new Node(val);
+            hashMap.insert( pair<string, Node*> (value, node));
+            isTerminal = false;
+        }
+
+        bool checkFor(string val)
+        {
+            map<string, Node*>::iterator check;
+            check = hashMap.find(val);
+
+            if(check == hashMap.end())
+            {
+            hashMap.erase(check);
+            return false;
+            }
+
+            else
+            {
+            hashMap.erase(check);
+            return true;
+            }
+        }
+
+        Node* getNode(string val)
+        {
+            return hashMap[val];
+        }
+
+        void removeNode(string val)
+        {
+            hashMap.erase(val);
+        }
+
+        string getNodeValue(string val)
+        {
+            return hashMap[val]->getValue();
+        }
+
+        void makeTerminal()
+        {
+            
+        }
+};
