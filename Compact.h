eea103cb77599d12bbd7430a194cef0562c326a9
[Frederick.Griffin] <[charliegside@gmail.com]>
2019-05-09 22:41:37 -0400

    converted char vals to string

--- Compact.h
+++ Compact.h
@@ -4,65 +4,19 @@
 #include <iostream>
 using namespace std; //std::cout;
 
-<<<<<<< HEAD
-class LetterNode{
- private:
-
-  string val;
-  bool isTerminal;
-  map <std::string, LetterNode*> hashMap;
-  int nodeCount;
-  char charVal;
- public:
-
-  LetterNode(string value)//check this line
-  //  this std::string value within the parameters gives an error
-  //  /*std::string value*/
-   {
-    this->val = value;
-    isTerminal = true;
-    nodeCount++;
-  }
-  char getVal(){
-    return charVal;
-  }
-
-  void setVal(string value){
-    //this std::string value within the parameters gives an error
-    ///*std::string value*
-    this->val = value;
-  }
-
-  void addNode(string value){
-    LetterNode* newestNode = new LetterNode(value);
-    hashMap->insert((newestNode); //hashMap was not declared in this scope
-    isTerminal= false;
-  }
-  void checkNode(std::string value){
-
-  }
-};
-
-
-class Compact{
- private:
-  LetterNode* head;
-=======
 class Compact{
  private:
 
-  //std::vector<char> arr; Didn't Grissom tell us not to do this?
->>>>>>> 1bec1ef82ae8389e546ad6ff1306b9b7a0840899
   int size;
 
  public:
 
-  Compact(char value) {
-
+  Compact(string value) {
+    size = 0;
   }
-  void add(char value);
+  void add(string value);
   void remove(int index);
-  bool checkWord(std::string word);
+  bool checkWord(string word);
   void printArr();
   int getSize();
   int trackSize();
