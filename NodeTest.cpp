95ce44df07a277c327dd3e79655cdd83134f0bb5
[Frederick.Griffin] <[charliegside@gmail.com]>
2019-05-12 21:43:59 -0400

    Run compile and cry

--- NodeTest.cpp
+++ NodeTest.cpp
@@ -1,20 +1,18 @@
 #include "Node.h"
 #include <iostream>
 using namespace std;
-class NodeTest{
   int main(){
-  CPPMap* map = new CPPMap();
-  cout<<"the initial size of the Map is: ";
-  cout<< map->syze();
-  string intial = "compute";
-  for(int b = 1; b==intial.size(); b++){
-    string addMe = intial.substr(b-1, b);
-    map->put(b, addMe);
-  }
-  cout<<"the entry values are: ";
-  cout<<map->entrySet();
-  cout<<"the key values are: ";
-  cout<<map->keySet();
-  return 0;
-  }
-};
+    CPPMap* map = new CPPMap();
+    cout<<"the initial size of the Map is: ";
+    cout<< map->syze();
+    string intial = "compute";
+    for(int b = 1; b==intial.size(); b++){
+      string addMe = intial.substr(b-1, b);
+      map->put(b, addMe);
+    }
+    cout<<"the entry values are: ";
+    cout<<map->entrySet();
+    cout<<"the key values are: ";
+    cout<<map->keySet();
+    return 0;
+    }
