36f96a9334d48384a8ea213b2133e0c41bd2137c
[Frederick.Griffin] <[charliegside@gmail.com]>
2019-05-12 21:34:13 -0400

    HALP

--- NodeTest.cpp
+++ NodeTest.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 class NodeTest{
   int main(){
-  CPPMap map = new CPPMap();
+  CPPMap* map = new CPPMap();
   cout<<"the initial size of the Map is: ";
   cout<< map->syze();
   string intial = "compute";
-  for(int i = 1, i=intial->size(), i++){
-    string addMe = intial->substr(i-1, i);
-    map.put(i, addMe);
+  for(int b = 1; b==intial.size(); b++){
+    string addMe = intial.substr(b-1, b);
+    map->put(b, addMe);
   }
-  cout<<"the entry values are: "
-  cout<<map.entrySet();
-  cout<<"the key values are: "
-  cout<<map.keySet();
-
-  }
+  cout<<"the entry values are: ";
+  cout<<map->entrySet();
+  cout<<"the key values are: ";
+  cout<<map->keySet();
   return 0;
+  }
 };
